d26eda380bfa71bbc144d0b94c789215f0fcc2d6
JeanCamargoReal <jeancamargoreal@icloud.com>
2022-12-27 15:51:56 -0300

    Iniciando projeto

---
+++ habit/habitApp.swift
@@ -0,0 +1,17 @@
+//
+//  habitApp.swift
+//  habit
+//
+//  Created by Jean Camargo on 21/12/22.
+//
+
+import SwiftUI
+
+@main
+struct habitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
