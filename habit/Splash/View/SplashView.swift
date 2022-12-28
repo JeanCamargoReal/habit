28e23d9ad7c0e5e0b0d4a52d09112f988003d111
JeanCamargoReal <jeancamargoreal@icloud.com>
2022-12-28 11:00:10 -0300

    Usando extensions em Views com SwiftUI

--- habit/Splash/View/SplashView.swift
+++ habit/Splash/View/SplashView.swift
@@ -14,13 +14,27 @@ struct SplashView: View {
     var body: some View {
         switch state {
         case .loading:
-            Text("Loading")
+            loadingView()
         case .goToSignInScreen:
             Text("carregar tela de login")
         case .goToHomeScreen:
             Text("carregar tela de home")
         case .error(let msg):
             Text("mostrar erro: \(msg)")
+        }
+    }
+}
+
+extension SplashView {
+    func loadingView() -> some View {
+        ZStack {
+            Image("logo")
+                .resizable()
+                .scaledToFit()
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .padding(20)
+                .background(Color.white)
+                .ignoresSafeArea()
         }
     }
 }
