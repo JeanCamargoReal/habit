46eb382019c3f7877d8aabcf2b0be4d9f1955be3
JeanCamargoReal <jeancamargoreal@icloud.com>
2023-02-01 15:57:30 -0300

    cria tela de login

--- habit/SignIn/View/SignInView.swift
+++ habit/SignIn/View/SignInView.swift
@@ -11,8 +11,97 @@ struct SignInView: View {
 
     @ObservedObject var viewModel: SignInViewModel
 
+    @State var email = ""
+    @State var password = ""
+    @State var action: Int? = 0
+    @State var navigationHidden = true
+
     var body: some View {
-        Text("Olá mundo")
-            .background(Color.blue)
+        NavigationView {
+            ScrollView(showsIndicators: false) {
+                VStack (alignment: .center, spacing: 20) {
+                    Spacer(minLength: 36)
+
+                    VStack(alignment: .center, spacing: 8) {
+                        Image("logo")
+                            .resizable()
+                            .scaledToFit()
+                            .padding(.horizontal, 48)
+
+                        Text("Login")
+                            .foregroundColor(.orange)
+                            .font(Font.system(.title).bold())
+                            .padding(.bottom, 8)
+
+                        numberField
+
+                        passwordField
+
+                        enterButton
+
+                        registerLink
+
+                        Text("Copyright @YYY")
+                            .foregroundColor(.gray)
+                            .font(Font.system(size: 16).bold())
+                            .padding(.top, 16)
+
+                    }
+                }
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .padding(.horizontal, 32)
+            .background(.white)
+            .navigationTitle("Login")
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationBarHidden(navigationHidden)
+        }
+    }
+}
+
+extension SignInView {
+    var numberField: some View {
+        TextField("", text: $email)
+            .border(.black)
+    }
+}
+
+extension SignInView {
+    var passwordField: some View {
+        SecureField("", text: $password)
+            .border(.orange)
+    }
+}
+
+extension SignInView {
+    var enterButton: some View {
+        Button("Entrar") {
+            //acontece o evendo de click
+        }
+    }
+}
+
+extension SignInView {
+    var registerLink: some View {
+        VStack {
+            Text("Ainda não possui um login ativo?")
+                .foregroundColor(.gray)
+                .padding(.top, 48)
+
+            ZStack {
+                NavigationLink(destination: Text("Tela de cadastro"), tag: 1, selection: $action, label: { EmptyView()})
+
+                Button("Realize seu cadastro") {
+                    self.action = 1
+                }
+            }
+        }
+    }
+}
+
+struct SignInView_Previews: PreviewProvider {
+    static var previews: some View {
+        let viewModel = SignInViewModel()
+        SignInView(viewModel: viewModel)
     }
 }
