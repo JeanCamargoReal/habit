2eef41aa64c8a7f83dd5aad30e03afa510617146
JeanCamargoReal <jeancamargoreal@icloud.com>
2023-07-19 15:05:52 -0300

    Tela de Splash no Dark Mode

--- habit/Splash/View/SplashView.swift
+++ habit/Splash/View/SplashView.swift
@@ -37,7 +37,6 @@ extension SplashView {
                 .scaledToFit()
                 .frame(maxWidth: .infinity, maxHeight: .infinity)
                 .padding(20)
-                .background(Color.white)
                 .ignoresSafeArea()
             
             if let error = error {
