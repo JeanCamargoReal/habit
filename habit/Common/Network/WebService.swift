2ebd32360f4792df726ebba3427741bbc665750f
JeanCamargoReal <jeancamargoreal@icloud.com>
2023-07-25 15:23:54 -0300

    WebService Genérico e Callbacks

--- habit/Common/Network/WebService.swift
+++ habit/Common/Network/WebService.swift
@@ -14,6 +14,18 @@ enum WebService {
 		case postUser = "/users"
 	}
 
+	enum NetworkError {
+		case badRequest
+		case notFound
+		case unautorized
+		case internalServerError
+	}
+
+	enum Result {
+		case success(Data)
+		case failure(NetworkError, Data?)
+	}
+
 	private static func completeUrl(path: Endpoint) -> URLRequest? {
 		guard let url = URL(string: "\(Endpoint.base.rawValue)\(path.rawValue)") else {
 			return nil
@@ -22,11 +34,11 @@ enum WebService {
 		return URLRequest(url: url)
 	}
 
-	static func postUser(request: SignUpRequest) {
+	private static func call<T: Encodable>(path: Endpoint, body: T, completion: @escaping (Result) -> Void ) {
 
-		guard let jsonData = try? JSONEncoder().encode(request) else { return } 
+		guard var urlRequest = completeUrl(path: path) else { return }
 
-		guard var urlRequest = completeUrl(path: .postUser) else { return }
+		guard let jsonData = try? JSONEncoder().encode(body) else { return }
 
 		urlRequest.httpMethod = "POST"
 		urlRequest.setValue("application/json", forHTTPHeaderField: "accept")
@@ -37,20 +49,43 @@ enum WebService {
 			guard let data = data, error == nil else {
 				print(error!)
 
+				completion(.failure(.internalServerError, nil))
+
 				return
 			}
 
-			print(String(data: data, encoding: .utf8)!)
+			if let r  = response as? HTTPURLResponse {
+				switch r.statusCode {
+					case 400:
+						completion(.failure(.badRequest, data))
+					case 200:
+						completion(.success(data))
+					default:
+						break
+				}
+			}
+
 
 			print("response\n")
 
 			print(response!)
-
-			if let r  = response as? HTTPURLResponse {
-				print(r.statusCode)
-			}
 		}
 
 		task.resume()
+	}
+
+	static func postUser(request: SignUpRequest) {
+		call(path: .postUser, body: request) { result in
+			switch result {
+				case .failure(let error, let data):
+					if let data = data {
+						print(String(data: data, encoding: .utf8)!)
+					}
+					break
+
+				case .success(let data):
+					break
+			}
+		}
 	}
 }
